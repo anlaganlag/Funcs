a79997b0f66575687087013ab120da6b12a4db87
gal <tanjhu@163.com>
2019-12-13 12:54:25 +0800

    efficent way to obtian sublist in gaps

---
+++ list_to_sublist(list_will_convert_to_sublist).py
@@ -0,0 +1,17 @@
+from itertools import zip_longest
+def list_range(lst,gap):
+    """
+     Efficent way handle list to sublist(gap you decide it)
+
+     Space unefficent way:[lst[i:i+gap] for i in lst[statr:end:gap]]
+    
+     zip() will abandon the last one if less than gap
+     
+     with zip_longest() the last one will plus Nones
+     """
+    tmp=[iter(lst)]*gap # create mutiple iterors
+    return list(zip_longest(*tmp))
+    
+
+print(list_range(range(2,11),4))
+[(2, 3, 4, 5), (6, 7, 8, 9), (10, None, None, None)]
