76fc2992164c85ad6edcb01e1d4a9570ce7ce1ac
anlaganlag <tanjhu@163.com>
2021-03-26 11:19:21 +0800

    斗地主改进带花色

--- 斗地主发牌325接近final.py
+++ 斗地主发牌325接近final.py
@@ -6,17 +6,17 @@ weight = {"3":1,"4":2,"5":3,"6":4,"7":5,"8":6,"9":7,"10":8,"11":9,"12":10,\
           "13":11,"1":12,"2":13,"14":14,"15":15}
 #各个牌型显示效果
 show = {"1":"A","10":"0","11":"J","12":"Q","13":"K","14":"X2","15":"X1"}
-
-
+#花色
+cardType = ["S","H","C","D"]
 
 #一副完整的新牌型
 def makeDeck():
     deck = []
     cardNum=52
     for i in range(cardNum):
-    #牌的点数即4个一组
-        deck.append(str(i//4+1))
-    #加上大小王
+    #牌的点数即4个一组,点数加上花色
+        deck.append(str(i//4+1)+" " +cardType[i%4])
+    #加上大小王14小王即X2,15大王即X1
     deck.extend(["14","15"])
     return deck
 
@@ -29,20 +29,25 @@ def makeShuffleDeck():
     total = 54
     for i in range(0,total,gap):
     #根据权重从大到小排序
-        ans.append(sorted(deck[i:i+gap],key=lambda x:-weight[x]))
+        ans.append(sorted(deck[i:i+gap],key=lambda x:-weight[x.split(" ")[0]]))
     h1,h2,h3,remain = ans
     return h1,h2,h3,remain
 
-def showIt(l):
 
+
+def showIt(l):
     ans = []
     for i in l:
-        if show.get(i):
+        if i in ["14","15"]:
             ans.append(show[i])
         else:
-            ans.append(i)
+            cardPoint,cardType= i.split(" ")
+            if show.get(cardPoint):
+                ans.append(show[cardPoint]+cardType)
+            else:
+                ans.append(cardPoint+cardType)
     return ans
             
 a1,a2,a3,r = makeShuffleDeck()
-showIt(a1)
-    
+print([showIt(a1),showIt(a2),showIt(a3),showIt(r)])
+
